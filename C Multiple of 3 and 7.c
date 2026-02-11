30001d81e78591ee0b35693a70c357eb88181da9
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-02-11 15:50:54 +0530

    Create C Multiple of 3 and 7.c

---
+++ C Multiple of 3 and 7.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a%3==0 && a%7==0) {
+        printf("Multiple of 3 and 7");
+    }
+    else {
+        printf("Not  Multiple of 3 and 7");
+    }
+    return 0;
+}
