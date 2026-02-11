240b30905743f7e44e775d2af0d2b4d566d5b73f
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-02-11 22:08:37 +0530

    Create C types of triangle.c

---
+++ C types of triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if ((a==b) && (b==c))
+    {
+        printf("Equlateral triangle");
+    }
+    else if ((a==b) || (b==c) || (c==a))
+    {
+        printf("Isosceles triangle ");
+    }
+    else {
+        printf("Scalene triangle ");
+    }
+
+    return 0;
+}
