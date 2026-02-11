76b1b81aaae044620812ebcb0f85ed3ec14815d8
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-02-11 20:58:17 +0530

    Create C Grade.c

---
+++ C Grade.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    if (a>=75 && a<=90)
+        {
+        printf("Grade B");
+    }
+    else
+    {
+        printf("Invalid Grade");
+    }
+        return 0;
+    }
