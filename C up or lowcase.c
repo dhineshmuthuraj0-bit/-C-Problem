e1545b2245424f256f5a66b8b2e39ee3634fb5df
Dhinesh M <dhineshmuthuraj0@gmail.com>
2026-02-11 21:25:42 +0530

    Create C up or lowcase.c

---
+++ C up or lowcase.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    char a;
+    scanf("%c",&a);
+    if (a>=65 && a<=90)
+        {
+        printf("Uppercase");
+    }
+    else
+    {
+        printf("Lowercase");
+    }
+        return 0;
+    }
